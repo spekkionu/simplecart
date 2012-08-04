8db83909c6339469a64884ecdad94a958d9d71ea
Jonathan Bernardi <spekkionu@spekkionu.com>
2012-08-03 17:51:16 -0700

    Update src/public_html/assets/scripts/jquery/jquery.flashmessage.js

--- src/public_html/assets/scripts/jquery/jquery.flashmessage.js
+++ src/public_html/assets/scripts/jquery/jquery.flashmessage.js
@@ -5,8 +5,7 @@
  * Uses jQuery UI classes by default
  * Positioning and size will need to be set on the element separately
  *
- * @author Jonathan Bernardi <jonathan@digitalcanvas.com>
- * @copyright Digital Canvas 2009
+ * @author Jonathan Bernardi
  * @version 1.1 10/14/2009
  */
 (function(b){b.fn.flashmessage=function(d){var a=b.extend({message:"",containerclass:"ui-widget",infoclass:"ui-state-highlight ui-corner-all",addicon:true,infoicon:"ui-icon ui-icon-info",speed:"slow",duration:2E3},d);return this.each(function(){var c=b(this);c.empty().hide();content=b('<div class="'+a.containerclass+'"></div>');content.append('<span class="'+a.infoicon+'"></span>');message=b('<span class="'+a.infoclass+'"></span>').html(a.message).appendTo(content);c.html(content);c.show(a.speed,
